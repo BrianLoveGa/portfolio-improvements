e2a274abca19256247e9d4891f8e76b6fa688ef7
Brian Loveless <Brian.loveless1@yahoo.com>
2019-11-23 16:24:06 -0500

    basics

--- portfolio/src/components/home.js
+++ portfolio/src/components/home.js
@@ -1,2 +1,13 @@
 import "./home.css"
 
+import React, { Component } from "react";
+
+export default class Home extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Brian's site here</h1>
+            </div>
+        )
+    }
+}
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('rooty'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,, document.getElementById('rooty'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
