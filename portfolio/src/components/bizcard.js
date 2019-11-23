2ea4063f9fc4c72ffacdabec43208ff857a3817f
Brian Loveless <Brian.loveless1@yahoo.com>
2019-11-23 16:47:24 -0500

    hmm

--- portfolio/src/components/bizcard.js
+++ portfolio/src/components/bizcard.js
@@ -7,6 +7,14 @@ export default class BizCard extends Component {
     return (
       <div className="biz">
         <h1>Brian's biz card digital here</h1>
+        <h1>resume download</h1>
+        <h2>linkedin</h2>
+        <h2>facebook</h2>
+        <h2>github</h2>
+        <h2>GA profile</h2>
+        <h2>codewars</h2>
+        <h2>dev to</h2>
+        <h2>you tube</h2>
       </div>
     );
   }
