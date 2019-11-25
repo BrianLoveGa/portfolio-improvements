24eaa393c0ff8a2fb84134c418093748d49c6856
Brian Loveless <Brian.loveless1@yahoo.com>
2019-11-25 13:28:05 -0500

    resume

--- portfolio/src/components/bizcard.js
+++ portfolio/src/components/bizcard.js
@@ -1,4 +1,5 @@
 import "./bizcard.css";
+import pdf from "./images/BrianRes.pdf"
 
 import React, { Component } from "react";
 
@@ -7,14 +8,88 @@ export default class BizCard extends Component {
     return (
       <div className="biz">
         <h1>My Digital Card</h1>
-        <h1>resume download</h1>
-        <h2>linkedin</h2>
-        <h2>facebook</h2>
-        <h2>github</h2>
-        <h2>GA profile</h2>
-        <h2>codewars</h2>
-        <h2>dev to</h2>
-        <h2>you tube</h2>
+        <h1>
+          {" "}
+          <a
+            download="Brian Resume.pdf"
+            href={pdf}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            Resume PDF Download{" "}
+          </a>
+        </h1>
+        <h2>
+          {" "}
+          <a
+            href="https://www.linkedin.com/in/brianloveless321bl"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </h2>
+        <h2>
+          {" "}
+          <a
+            href="https://github.com/BrianLoveGa"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </h2>
+        <h2>
+          {" "}
+          <a
+            href="https://profiles.generalassemb.ly/profiles/brian-loveless"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GA Profile
+          </a>
+        </h2>
+        <h2>
+          {" "}
+          <a
+            href="https://www.codewars.com/users/BrianLoveless1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Code Wars
+          </a>
+        </h2>
+        <h2>
+          {" "}
+          <a
+            href="https://dev.to/brianlovega"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            DEV .To
+          </a>
+        </h2>
+        <h2>
+          {" "}
+          <a
+            href="https://www.youtube.com/channel/UCDhV9ljaaONARojllOzuw5g"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            YouTube
+          </a>
+        </h2>
+        <h2>
+          {" "}
+          <a
+            href="https://www.facebook.com/brian.loveless.90"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Facebook
+          </a>
+        </h2>
       </div>
     );
   }
