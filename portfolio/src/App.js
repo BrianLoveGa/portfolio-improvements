1797d217b6ed1914fec68b8f3c5f448967cea34a
Brian Loveless <Brian.loveless1@yahoo.com>
2019-11-25 10:26:40 -0500

    routes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -21,22 +21,22 @@ function App() {
             <span></span>
             <ul id="menu">
               <li>
-                <Link className="link" to="/home">
+                <Link className="link" to="/portfolio-improvements/home">
                   Home
                 </Link>
               </li>
               <li>
-                <Link className="link" to="/about">
+                <Link className="link" to="/portfolio-improvements/about">
                   About
                 </Link>
               </li>
               <li>
-                <Link className="link" to="/biz">
+                <Link className="link" to="/portfolio-improvements/biz">
                   Biz Card
                 </Link>
               </li>
               <li>
-                <Link className="link" to="/projects">
+                <Link className="link" to="/portfolio-improvements/projects">
                   Projects
                 </Link>
               </li>
@@ -48,13 +48,29 @@ function App() {
       <main>
         <div>
           <Switch>
-            <Route path="/home" exact={true} component={Home} />{" "}
+            <Route
+              path="/portfolio-improvements/home"
+              exact={true}
+              component={Home}
+            />{" "}
             <Route exact path="/">
-              <Redirect to="/home" />
+              <Redirect to="/portfolio-improvements/home" />
             </Route>
-            <Route path="/about" exact={true} component={About} />{" "}
-            <Route path="/biz" exact={true} component={BizCard} />{" "}
-            <Route path="/projects" exact={true} component={Projects} />{" "}
+            <Route
+              path="/portfolio-improvements/about"
+              exact={true}
+              component={About}
+            />{" "}
+            <Route
+              path="/portfolio-improvements/biz"
+              exact={true}
+              component={BizCard}
+            />{" "}
+            <Route
+              path="/portfolio-improvements/projects"
+              exact={true}
+              component={Projects}
+            />{" "}
           </Switch>
         </div>
       </main>
