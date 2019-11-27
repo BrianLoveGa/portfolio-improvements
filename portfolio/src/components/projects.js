12d2cdf035e1a8349751a26efbcaed27df9d7e26
Brian Loveless <Brian.loveless1@yahoo.com>
2019-11-27 13:33:16 -0500

    links added

--- portfolio/src/components/projects.js
+++ portfolio/src/components/projects.js
@@ -13,6 +13,8 @@ export default class Projects extends Component {
     return (
       <div className="projpage">
         <h1>My Projects </h1>
+        <h3>All sites are mobile friendly </h3>
+        <h3>click the link under the slideshow </h3>
         <div className="projectsbox">
           <div className="project">
             <h1> Project 1 </h1>
@@ -20,13 +22,21 @@ export default class Projects extends Component {
             <div className="projPic">
               <Proj1Slides />
             </div>
-            <p>Where I created a simple game in Pyhton3 and the same game
-              in a JavaScript prompts version to compare and contrast them.
-              Also a React buttons and prompts version. The plan is to take 
-              this simple template and grow it into something more entertaining.
-              You can also check the weather, and look at random dog pics.
-          
+            <p>
+              Where I created a simple game in Pyhton3 and the same game in a
+              JavaScript prompts version to compare and contrast them. Also a
+              React buttons and prompts version. The plan is to take this simple
+              template and grow it into something more entertaining. You can
+              also check the weather, and look at random dog pics.
             </p>
+            <a
+              className="plink"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://brianlovega.github.io/proj4-simple-over-complex/"
+            >
+              Visit Simple > Complex here
+            </a>
           </div>
           <div className="project">
             <h1> Project 2 </h1>
@@ -34,12 +44,22 @@ export default class Projects extends Component {
             <div className="projPic">
               <Proj2Slides />
             </div>
-            <p> A group project where we wanted to create an essy to use site 
-              where recipies could be easily shared. Heroku deployed MongoDB backend
-              and a React front-end via Github pages with RESTful routes and full
-              CRUD (create read update delete). MERN stack 
-              (mongoose, express, react, node).
+            <p>
+              {" "}
+              A group project where we wanted to create an essy to use site
+              where recipies could be easily shared. Heroku deployed MongoDB
+              backend and a React front-end via Github pages with RESTful routes
+              and full CRUD (create read update delete). MERN stack (mongoose,
+              express, react, node).
             </p>
+            <a
+              className="plink"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://esin87.github.io/ga-seir-project3-frontend/"
+            >
+              Share a recipie here
+            </a>
           </div>
           <div className="project">
             <h1> Project 3 </h1>
@@ -47,10 +67,19 @@ export default class Projects extends Component {
             <div className="projPic">
               <Proj3Slides />
             </div>
-            <p>A fun game to pass the time.
-               Play rock paper scissors against the computer.
-               Just press the button enter your move and feel 
-               the glory of victory, or the sting of defeat. </p>
+            <p>
+              A fun game to pass the time. Play rock paper scissors against the
+              computer. Just press the button enter your move and feel the glory
+              of victory, or the sting of defeat.{" "}
+            </p>
+            <a
+              className="plink"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://brianlovega.github.io/game-rock-paper-scissors/"
+            >
+              Play the RPSLS game here
+            </a>
           </div>
           <div className="project">
             <h1> Project 4 </h1>
@@ -58,9 +87,18 @@ export default class Projects extends Component {
             <div className="projPic">
               <Proj4Slides />
             </div>
-            <p>An homage to the old school link pages.
-              A site of pure CSS and HTML .
-              Fun sites for new and old coders alike to check out. </p>
+            <p>
+              An homage to the old school link pages. A site of pure CSS and
+              HTML . Fun sites for new and old coders alike to check out.{" "}
+            </p>
+            <a
+              className="plink"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://brianlovega.github.io/cool-LinksPage/"
+            >
+              Check it out here
+            </a>
           </div>
         </div>
       </div>
