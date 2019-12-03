313f06d9ee3144b0d3b7a4ac107d7ed398872c30
Brian Loveless <Brian.loveless1@yahoo.com>
2019-12-02 21:43:49 -0500

    fcc css

--- portfolio/src/components/codecamp.js
+++ portfolio/src/components/codecamp.js
@@ -12,8 +12,31 @@ export default class Codecamp extends Component {
           site
         </h3>
         <br></br>
-        <h3>Code camp homepage link holder</h3>
-        <h3>my code camp profile link holder</h3>
+        <div className="campbox">
+          <h3>
+            Code camp homepage{" "}
+            <a
+              href="https://www.freecodecamp.org/learn"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              link here{" "}
+            </a>{" "}
+            to their site.
+          </h3>
+          <h3>
+            To see my code camp profile{" "}
+            <a
+              href="https://www.freecodecamp.org/fcc834d7277-a991-4083-bda5-6372b69a9f1e"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              click here{" "}
+            </a>{" "}
+            to go there.
+          </h3>
+        </div>
+
         <div className="proj1_box">
           <h3>First project for responsive web design</h3>
           <h3>"make a tribute page" via code pen</h3>
@@ -22,7 +45,37 @@ export default class Codecamp extends Component {
             src={require("./images/projs/codecamp/HST-tribute.png")}
             alt="Hunter S Thompson face close up with classic yellow shades"
           ></img>
-          <h3>Link to completed project page on code pen holder</h3>
+          <h3>
+            Link to completed project page{" "}
+            <a
+              href="https://codepen.io/B-code-Loveless/pen/abzbyQr?editors=1100"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              avaliable here{" "}
+            </a>{" "}
+            on code pen site.
+          </h3>
+        </div>
+        <div className="proj1_box">
+          <h3>Second project for responsive web design</h3>
+          <h3>"make a survey page" via code pen</h3>
+          <img
+            class="ccproj1"
+            // src={require("./images/projs/codecamp/#")}
+            alt="Hunter S Thompson face close up with classic yellow shades"
+          ></img>
+          <h3>
+            Link to completed project page{" "}
+            <a
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              avaliable here{" "}
+            </a>{" "}
+            on code pen site.
+          </h3>
         </div>
       </div>
     );}}
