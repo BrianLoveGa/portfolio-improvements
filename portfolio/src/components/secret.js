264303e9b999bfd33efa0458e21947f736efde0b
Brian Loveless <Brian.loveless1@yahoo.com>
2019-12-05 20:53:00 -0500

    calling it for now

--- portfolio/src/components/secret.js
+++ portfolio/src/components/secret.js
@@ -20,11 +20,24 @@ export default class Secret extends Component {
           </div>
         </div>
         <p>
-          <a href="https://giphy.com/gifs/cbc-funny-comedy-xUA7aLpVxPVEoEPXji">
+          <a
+            className="glink"
+            href="https://giphy.com/gifs/cbc-funny-comedy-xUA7aLpVxPVEoEPXji"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             via GIPHY
           </a>
         </p>
         <h2>Come on in I assure you we are open !</h2>
+        <h3>Now taking orders for </h3>
+        <div className="openlist">
+          <ul className="openlist">
+            <li>Custom Website Creation</li>
+            <li>New Website Design and Development</li>
+            <li>Updating exisitng Sites or Apps</li>
+          </ul>
+        </div>
       </div>
     );
   }
