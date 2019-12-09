43397724775b7004b3638e4f76248db2a270fa93
Brian Loveless <Brian.loveless1@yahoo.com>
2019-12-09 12:29:27 -0500

    css

--- portfolio/src/components/more.js
+++ portfolio/src/components/more.js
@@ -8,6 +8,7 @@ export default class More extends Component {
         <div className="morepage">
             <h1>MORE COMING SOON!</h1>
             <h3>congrats on finding this secret page</h3>
+            <h3>Now go look at something better</h3>
         </div>
     )
   }
