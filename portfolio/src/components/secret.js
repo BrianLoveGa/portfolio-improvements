476d5a3c6bf8521fd556c93b5387b678da63dbf5
Brian Loveless <Brian.loveless1@yahoo.com>
2020-02-23 16:27:45 -0500

    card on secrect page

--- portfolio/src/components/secret.js
+++ portfolio/src/components/secret.js
@@ -3,7 +3,6 @@ import "./secret.css";
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-
 export default class Secret extends Component {
   render() {
     return (
@@ -38,6 +37,47 @@ export default class Secret extends Component {
             <li>New Website Design and Development</li>
             <li>Updating exisitng Sites or Apps</li>
           </ul>
+        </div>
+        <div className="cards">
+          <h2>I have recently made some static site holiday cards</h2>
+          <h3>Valentine's for my Sweet-Heart</h3>
+          <div></div>
+          <h4>HTML & CSS static site responsive images and media</h4>
+          <h4>designed for mobile viewing</h4>
+          <img
+            src={require("./images/vday.png")}
+            className="responsive"
+            alt="picture of rose galaxy and a picture of a dog holding a flower with text above galaxy photo and between the two"
+          />
+          <p>
+            <a
+              className="holiday-card"
+              href="https://brianlovega.github.io/ilovemichellefryer/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              V-Day Card
+            </a>
+          </p>
+          <h3>Birthday for my Dad's 70th</h3>
+          <div></div>
+          <h4>HTML & CSS static site responsive video and images</h4>
+          <h4>designed for mobile viewing</h4>
+          <img
+            src={require("./images/dad-bday-card.png")}
+            className="responsive"
+            alt="picture of me and dad on couch with our dogs with text above photo : he's a dog gone great dad and sports news from 1950 underneath"
+          />
+          <p>
+            <a
+              className="holiday-card"
+              href="https://brianlovega.github.io/Happy-B-DAY_POPS/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              B-Day Card
+            </a>
+          </p>
         </div>
         <div className="wiz">
           <h2>I am not claiming to be a full fledged "4 W" just yet</h2>
