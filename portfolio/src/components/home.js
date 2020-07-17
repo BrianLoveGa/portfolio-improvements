3d3c6ce2687426caab25580d1e436ab5fa67e678
Brian Loveless <Brian.loveless1@yahoo.com>
2020-07-17 11:49:45 -0400

    maintain

--- portfolio/src/components/home.js
+++ portfolio/src/components/home.js
@@ -1,15 +1,15 @@
 import "./home.css";
 
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
-import Proj1Slides from "./proj1slide";
-import Proj2Slides from "./proj2slide";
-import Proj3Slides from "./proj3slide";
-import Proj4Slides from "./proj4slide";
-import Proj5Slides from "./proj5slide";
-import Proj6Slides from "./proj6slides";
-import Proj7Slides from "./proj7slides";
+// import Proj1Slides from "./proj1slide";
+// import Proj2Slides from "./proj2slide";
+// import Proj3Slides from "./proj3slide";
+// import Proj4Slides from "./proj4slide";
+// import Proj5Slides from "./proj5slide";
+// import Proj6Slides from "./proj6slides";
+// import Proj7Slides from "./proj7slides";
 
 import About from "./about";
 import BizCard from "./bizcard";
