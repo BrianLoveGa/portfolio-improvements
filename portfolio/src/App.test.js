bac3c5bd8b51ddc5c169bb930502ffe4f13e2d54
Brian Loveless <Brian.loveless1@yahoo.com>
2020-07-26 14:53:11 -0400

    testing back working

--- portfolio/src/App.test.js
+++ portfolio/src/App.test.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("it is a computer right", () => {
+  it("can do math", () => {
+    expect(40 + 2).toEqual(42);
+    console.log("40 + 2 equals 42");
+  });
 });
